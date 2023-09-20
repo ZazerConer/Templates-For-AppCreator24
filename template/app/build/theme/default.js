1580846f0ac29a8138e5cef473c480945370936d
ZazerConer's <96803085+ZazerConer@users.noreply.github.com>
2023-09-21 06:32:02 +0800

    Create default.js

---
+++ template/app/build/theme/default.js
@@ -0,0 +1,32 @@
+/** 
+  * Templates for AppCreator24 @ ZazerConer's GitHub
+  * Build your app easily.
+  * JS code.
+  * App theme display color.
+  * Preferred theme: Default color.
+  * You can change the theme with your favorite color.
+**/
+
+  // Top navigation.
+$("#top-navigation").css("background", "#5C6BC0");
+  // Top navigation: icon.
+$("#top-navigation button").css("color", "#E8EAF6");
+  // Top navigation: title.
+$("#app-name").css("color", "#E8EAF6");
+  // Bottom navigation.
+$("#bottom-navigation").css("background", "#5C6BC0");
+  // Bottom navigation: icon.
+$("#bottom-navigation .bi").css("color", "#E8EAF6");
+  // Menu.
+$("#container-menu").css("background", "#6E7BBE");
+  // Menu: section button.
+$("#container-menu button").css("color", "#E8EAF6");
+  // Menu: section icon.
+$("#container-menu .bi").css("color", "#E8EAF6");
+  // Content.
+$("section").css({"color": "#5C6BC0", "background": "#E8EAF6"});
+  // Loading section.
+$("#loading").css("background", "#3F51B5");
+  // body.
+ var bodyColor = document.body;
+$(bodyColor).css("background", "#5C6BC0");
